538931c9a9d8961ef4ba9b42bad0ac515f170c7d
Chip Audette <chipaudette@yahoo.com>
2017-01-02 08:46:44 -0500

    Add AudioMultiply and AudioMixer to lib header and to keywords

--- OpenAudio_ArduinoLibrary.h
+++ OpenAudio_ArduinoLibrary.h
@@ -1,7 +1,9 @@
 
 #include <AudioStream_F32.h>
+
+#include <AudioControlSGTL5000_Extended.h>
 #include <AudioConvert_F32.h>
 #include <AudioEffectGain_F32.h>
 #include <AudioEffectCompressor_F32.h>
-#include <AudioControlSGTL5000_Extended.h>
-
+#include <AudioMixer4_F32.h>
+#include <AudioMultiply_F32.h>
