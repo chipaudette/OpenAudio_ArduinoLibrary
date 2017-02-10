a01405ca0fecdc2a65b2ab407fdf6e5321078637
Chip Audette <chipaudette@yahoo.com>
2017-02-10 12:16:55 -0500

    Add FIR and IIR to OpenAudio library header

--- OpenAudio_ArduinoLibrary.h
+++ OpenAudio_ArduinoLibrary.h
@@ -1,10 +1,11 @@
 
 #include <AudioStream_F32.h>
-
 #include <AudioControlSGTL5000_Extended.h>
 #include <AudioConvert_F32.h>
 #include <AudioEffectGain_F32.h>
 #include <AudioEffectCompressor_F32.h>
+#include <AudioFilterFIR_F32.h>
+#include <AudioFilterIIR_F32.h>
 #include <AudioMixer4_F32.h>
 #include <AudioMultiply_F32.h>
 #include <synth_waveform_F32.h>
