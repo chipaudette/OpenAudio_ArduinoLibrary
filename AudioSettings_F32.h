cce309ffd299c512d32092810b3fd6828844f286
joerg <joerg.hohensohn@gmx.de>
2022-05-15 11:42:38 +0200

    have defaults for AudioSettings_F32

--- AudioSettings_F32.h
+++ AudioSettings_F32.h
@@ -2,9 +2,11 @@
 #ifndef _AudioSettings_F32_
 #define _AudioSettings_F32_
 
+#include <AudioStream.h> // for AUDIO_SAMPLE_RATE_EXACT, AUDIO_BLOCK_SAMPLES
+
 class AudioSettings_F32 {
 	public:
-		AudioSettings_F32(float fs_Hz, int block_size) :
+		AudioSettings_F32(float fs_Hz=AUDIO_SAMPLE_RATE_EXACT, int block_size=AUDIO_BLOCK_SAMPLES) :
 			sample_rate_Hz(fs_Hz), audio_block_samples(block_size) {}
 		const float sample_rate_Hz;
 		const int audio_block_samples;
