6997a2bb220728dc97b9fd7b3a92657e35cee52e
Julia M Stephenson <52968628+julia-m-stephenson@users.noreply.github.com>
2026-07-11 15:06:56 +0100

    Can now change sample frequency
    TAA and PCM1808/PCM5102 all seem to work at 192000 on Teensy 4.1

--- examples/PassThroughQuadF32/PassThroughQuadF32.ino
+++ examples/PassThroughQuadF32/PassThroughQuadF32.ino
@@ -7,8 +7,13 @@
 #include <Audio.h>
 #include <OpenAudio_ArduinoLibrary.h>
 #include <AudioStream_F32.h>
+#include <utility/imxrt_hw.h> 
 
-const int sample_rate_Hz = 48000;
+// T3.x supported sample rates: 2000, 8000, 11025, 16000, 22050, 24000, 32000, 44100, 44117, 48000,
+//                             88200, 88235 (44117*2), 95680, 96000, 176400, 176470, 192000
+// T4.x supports any sample rate the codec will handle.
+
+const int sample_rate_Hz = 192000;
 const int audio_block_samples = 128;   // Always 128
 
 AudioControlSGTL5000 sgtl5000_1;
@@ -71,12 +76,14 @@ void setup() {
   digitalWrite(MUTE, UNMUTEAUDIO);  // Keep audio junk out of the speakers/headphones until configuration is complete.
 
  #endif
+   setI2SFreq(sample_rate_Hz);
 
 }
 
 void loop() {
  
   Serial.printf("PassThrough Running Now\n");
+  Serial.printf("Sample Rate = %d\n",sample_rate_Hz);
 
   if (myInput == AUDIO_INPUT_MIC){
     Serial.printf("Now using mic!\n");
@@ -99,3 +106,18 @@ void loop() {
 //
 
 }
+// Frank B's routine for setting I2S clocks. *** FOR T4.x ONLY ***
+void setI2SFreq(int freq1)    // Thank ypu, Frank B.
+   {
+   // PLL between 27*24 = 648MHz und 54*24=1296MHz
+   int n1 = 4; //SAI prescaler 4 => (n1*n2) = multiple of 4
+   int n2 = 1 + (24000000 * 27) / (freq1 * 256 * n1);
+   double C = ((double)freq1 * 256 * n1 * n2) / 24000000;
+   int c0 = C;
+   int c2 = 10000;
+   int c1 = C * c2 - (c0 * c2);
+   set_audioClock(c0, c1, c2, true);
+   CCM_CS1CDR = (CCM_CS1CDR & ~(CCM_CS1CDR_SAI1_CLK_PRED_MASK | CCM_CS1CDR_SAI1_CLK_PODF_MASK))
+       | CCM_CS1CDR_SAI1_CLK_PRED(n1-1)                                // &0x07
+       | CCM_CS1CDR_SAI1_CLK_PODF(n2-1);                               // &0x3f 
+   }
